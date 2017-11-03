13d97199dd8425069fc4d4559f9b3ebd1d82f3b7
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-11-03 11:35:53 -0400

    add contact.erb, contact entry in app.rb

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -22,13 +22,13 @@ describe 'App' do
   it "/about renders the about page dog" do
     get '/about'
     expect(last_response).to be_ok
-    expect(last_response.body).to include('About')
+    expect(last_response.body).to include('<h1>About</h1>')
   end
 
   it "/contact renders the contact page dog" do
     get '/contact'
     expect(last_response).to be_ok
-    expect(last_response.body).to include('Contact')
+    expect(last_response.body).to include('<h1>Contact</h1>')
   end
 
 
